71b375aacbb54c5f8c07034754dd5c1bee3a2ac5
Timvanhal <s1115001@student.hsleiden.nl>
2019-10-07 14:02:25 +0200

    -Added main class. -Setup project

---
+++ java/src/AppMain.java
@@ -0,0 +1,22 @@
+package src;
+
+import javafx.application.Application;
+import javafx.stage.Stage;
+
+
+/**
+ * Main class for executing application.
+ *
+ * @author Timvhal
+ */
+public class AppMain extends Application {
+
+    @Override
+    public void start(Stage primaryStage) throws Exception {
+        //do something.
+    }
+
+    public static void main(String[] args) {
+        launch(args);
+    }
+}
