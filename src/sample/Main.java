d91e550c43e74b60e61c0a2e7bfb18e2b9110863
Kythisto <50318815+Kythisto@users.noreply.github.com>
2019-10-08 18:40:37 +0200

    Kan scenes switchen

--- src/sample/Main.java
+++ src/sample/Main.java
@@ -6,18 +6,20 @@ import javafx.scene.Parent;
 import javafx.scene.Scene;
 import javafx.stage.Stage;
 
+import java.io.IOException;
+
 public class Main extends Application {
 
     @Override
     public void start(Stage primaryStage) throws Exception{
-        Parent root = FXMLLoader.load(getClass().getResource("sample.fxml"));
+        Parent root = FXMLLoader.load(getClass().getResource("Overview.fxml"));
         primaryStage.setTitle("Kilometer Registratie");
         primaryStage.setScene(new Scene(root, 1200, 900));
         primaryStage.show();
     }
 
 
-    public static void main(String[] args) {
+    public static void main(String[] args)throws IOException {
         launch(args);
     }
 }
