f8c397e157e913e0f0397d4c89e65f725cdeaa57
Stan <hogenboomstan@gmail.com>
2019-10-09 15:06:06 +0200

    fixed the fxml

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -12,7 +12,7 @@ public class Main extends Application {
 
     @Override
     public void start(Stage primaryStage) throws Exception{
-        Parent root = FXMLLoader.load(getClass().getResource("overview.fxml"));
+        Parent root = FXMLLoader.load(getClass().getResource("../res/views/overview.fxml"));
         primaryStage.setTitle("Kilometer Registratie");
         primaryStage.setScene(new Scene(root, 1200, 900));
         primaryStage.show();
