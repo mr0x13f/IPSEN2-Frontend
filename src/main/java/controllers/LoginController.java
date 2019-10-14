4675fdf00f26c7b0dfd2474cea7933b56bd6915d
Stan <hogenboomstan@gmail.com>
2019-10-14 13:31:08 +0200

    Added login screen
    Tab views are now available

--- src/main/java/controllers/LoginController.java
+++ src/main/java/controllers/LoginController.java
@@ -1,6 +1,41 @@
 package main.java.controllers;
 
+import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.scene.control.Label;
+import javafx.scene.control.TabPane;
+import javafx.stage.Stage;
+
+import java.io.IOException;
+
 public class LoginController {
+
+    private static LoginController lController;
+
+    @FXML
+    public  TabPane masterView;
+
+    static {
+        lController = new LoginController();
+    }
+
+    private LoginController() {
+
+    }
+
+    public static LoginController getInstance() {
+        return lController;
+    }
+
+    @FXML
+    public  void OpenMasterView() throws IOException {
+        Stage stage = (Stage) masterView.getScene().getWindow();
+        Parent overviewScene = FXMLLoader.load(getClass().getResource("../../res/views/masterView.fxml"));
+        stage.setScene(new Scene(overviewScene, 1200, 900));
+    }
+
 
     public void Authencate(){}
 
--- src/main/java/views/LoginView.java
+++ src/main/java/views/LoginView.java
@@ -1,9 +1,16 @@
 package main.java.views;
 
+import javafx.fxml.FXML;
+import javafx.scene.layout.AnchorPane;
+import main.java.controllers.LoginController;
+
+import java.io.IOException;
+
 public class LoginView {
+    private LoginController loginController = LoginController.getInstance();
 
-    private void login(){
-
+    private void login() throws IOException {
+        loginController.OpenMasterView();
     }
 
     private void createUser(){
