3d5998b3bb811aff7c90b8a3e5b6f4a1d5137867
MartijnSteen <martijnvandersteen1999@gmail.com>
2019-10-30 12:55:55 +0100

    json proberen - martijn

--- src/main/java/controllers/ManagementTabController.java
+++ src/main/java/controllers/ManagementTabController.java
@@ -1,42 +1,89 @@
 package main.java.controllers;
 
+import com.google.gson.Gson;
+import com.google.gson.JsonObject;
+import com.google.gson.stream.JsonWriter;
 import javafx.fxml.FXML;
 import javafx.scene.control.Tab;
-import main.java.models.Company;
-import main.java.models.Project;
+import jdk.nashorn.internal.runtime.Version;
+import main.java.models.*;
 import main.java.models.Rate;
-import main.java.models.Rate;
-import main.java.models.Vehicle;
 import main.java.models.Vehicle;
 import main.java.observers.ManagementTabObserver;
+
+import java.io.FileWriter;
+import java.io.IOException;
+import java.io.Writer;
 
 public class ManagementTabController {
     Company ComModel = new Company();
     Project ProModel = new Project();
     Rate RateModel = new Rate();
-    Vehicle CarModel = new Vehicle();
+
+    String jsonPath = "main\\res\\JSON\\Cars.json";
+
+    private String jsonfile;
+
+
 
     @FXML private Tab managementTab;
 
+    Gson gson = new Gson();
 
 
-    public void saveJourney(){}
+
+//    public void addToCarToGson(String carName, String licensePlate){
+//        Vehicle vehiclejson = new Vehicle(carName, licensePlate);
+//
+//        String json = gson.toJson(vehiclejson);
+//        System.out.println("dit is de JSON: "+ json);
+//
+//        Writer fileName = new Writer() {
+//            @Override
+//            public void write(char[] cbuf, int off, int len) throws IOException {
+//
+//            }
+//
+//            @Override
+//            public void flush() throws IOException {
+//
+//            }
+//
+//            @Override
+//            public void close() throws IOException {
+//
+//            }
+//        };
+//        try(JsonWriter file = new JsonWriter(fileName)){
+//
+//
+//        } catch (Exception e){
+//            System.out.println("gevangen");
+//        }
+//
+//    }
+
+    public void saveJourney(){
+
+    }
 
 
-    public void CombineDetails(String Company, String Project, float Rate) {
+    public void combineDetails(String Company, String Project, float Rate) {
         ComModel.yourCompany(Company);
         ProModel.yourProject(Project);
         RateModel.yourRate(Rate);
     }
 
-    public void CombineCarDetails(String carName, String licenseCar) {
-        CarModel.yourLicensePlate(licenseCar);
-        CarModel.yourCarName(carName);
-    }
 
-    public void register(ManagementTabObserver mto) {
-        CarModel.register(mto);
-    }
+//    OBSERVER PATERN
+//    public void combineCarDetails(String carName, String licenseCar) {
+//        CarModel.yourLicensePlate(licenseCar);
+//        CarModel.yourCarName(carName);
+//    }
+//
+//    public void register(ManagementTabObserver mto) {
+//        CarModel.register(mto);
+//    }
 
 
 }
